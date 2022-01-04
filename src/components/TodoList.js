d48f66e5fd9e0b6a642464c3b8e7e495eec1244a
Daniel Rivas <djra26111990@gmail.com>
2022-01-04 13:06:33 -0500

    Fix Index counter state & add to LocalStorage Index

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,23 +2,32 @@ import React from "react";
 import { TodoItem } from "./TodoItem";
 import { ButtonAction } from "./ButtonAction";
 import * as actions from "../redux/actions/actions";
-import { useStateRedux } from '../hooks/useStateRedux'
-import { useTodosValues } from '../hooks/useTodosValues'
+import { useStateRedux } from "../hooks/useStateRedux";
+import { useTodosValues } from "../hooks/useTodosValues";
 
-let indexTodo = 0;
 export const TodoList = () => {
-  const { todo, setTodo, error, setError, loadingData } = useTodosValues()
-  const { dispatch, todos } = useStateRedux()
+  const {
+    todo,
+    setTodo,
+    error,
+    setError,
+    loadingData,
+    indexTodo,
+    setIndexTodo,
+  } = useTodosValues();
+  const { dispatch, todos } = useStateRedux();
   const addTodoToList = actions.addTodoAction;
   const deleteTodoList = actions.deleteTodoListAction;
-  
+  let index = indexTodo;
   const handleChange = (e) => {
     setTodo({
       ...todo,
-      id: indexTodo++,
+      id: index++,
       todo: e.target.value,
       isComplete: false,
     });
+    setIndexTodo(index);
+    localStorage.setItem("indexTodo", indexTodo);
   };
   const handleAddTodo = (e) => {
     e.preventDefault();
@@ -38,24 +47,39 @@ export const TodoList = () => {
     dispatch(deleteTodoList());
   };
 
+  let styleBtn = "p-4 m-2 rounded border-2 ring-0 text-white";
+  let styleBtnAddTodo = `${styleBtn} bg-green-500 hover:bg-green-700`;
+  let styleBtnDeleteTodos = `${styleBtn} bg-red-500 hover:bg-red-700`;
+  let styleBtnUpdateTodo = `${styleBtn} bg-green-500 hover:bg-green-700 columns-3`;
+  let styleBtnDeleteTodo = `${styleBtn} bg-red-500 hover:bg-red-700 columns-3`;
+
   return (
-    <div style={{ display: 'flex'}}>
+    <div>
       {loadingData ? (
         "Loading..."
       ) : (
-        <div style={{ padding: '5px', flexDirection: 'column'}}>
+        <div className="container mx-auto px-4 text-center">
           <input
-            style={{ padding: '5px'}}
+            className="p-4 border-2 mx-auto sm:p-2"
             id="todo"
             onChange={handleChange}
             type="text"
             placeholder="Enter a todo"
           />
-          {error ? <h2 style={{ color: '#ff3333', background: '#ff9999', padding: '10px'}}>No puede estar vacio el campo</h2> : null}
-          <ButtonAction onClick={handleAddTodo}>Add Todo</ButtonAction>
-          <ButtonAction onClick={handleDeleteTodoList}>
+          <ButtonAction className={styleBtnAddTodo} onClick={handleAddTodo}>
+            Add Todo
+          </ButtonAction>
+          <ButtonAction
+            className={styleBtnDeleteTodos}
+            onClick={handleDeleteTodoList}
+          >
             Delete Todo List
           </ButtonAction>
+          {error ? (
+            <div className="shadow-md p-4 bg-red-400 w-auto m-2">
+              <h2 className="text-red-600">No puede estar vacio el campo</h2>
+            </div>
+          ) : null}
           {todos.map((elem, index) => {
             return (
               <TodoItem
@@ -63,6 +87,7 @@ export const TodoList = () => {
                 isComplete={elem.isComplete}
                 updateButton={
                   <ButtonAction
+                    className={styleBtnUpdateTodo}
                     onClick={() => dispatch(actions.updateTodoAction(elem))}
                   >
                     Update Todo
@@ -70,6 +95,7 @@ export const TodoList = () => {
                 }
                 deleteButton={
                   <ButtonAction
+                    className={styleBtnDeleteTodo}
                     onClick={() => dispatch(actions.deleteTodoAction(elem))}
                   >
                     Delete Todo
--- src/hooks/useTodosValues.js
+++ src/hooks/useTodosValues.js
@@ -6,12 +6,16 @@ export const useTodosValues = () => {
   const [todo, setTodo] = useState({ id: null, todo: "", isComplete: false });
   const [error, setError] = useState(false);
   const [loadingData, setLoadingData] = useState(false);
+  const [indexTodo, setIndexTodo] = useState(0)
   const getTodoList = actions.getTodosAction;
   
   useEffect(() => {
     setLoadingData(true);
     let savedState = JSON.parse(localStorage.getItem("todos"));
-
+    let index = JSON.parse(localStorage.getItem('indexTodo'))
+    index++
+    index === null ? setIndexTodo(0) : setIndexTodo(index)
+    
     if (!savedState) {
       setLoadingData(false);
       return;
@@ -19,7 +23,7 @@ export const useTodosValues = () => {
       dispatch(getTodoList(savedState));
       setLoadingData(false);
     }
-  }, [dispatch, getTodoList]);
+  }, [dispatch, getTodoList, indexTodo]);
 
-  return { todo, setTodo, error, setError, loadingData };
+  return { todo, setTodo, error, setError, loadingData, indexTodo, setIndexTodo };
 };
